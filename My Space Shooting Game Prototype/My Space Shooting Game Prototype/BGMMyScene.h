1154a197304e3bce11ca7be0398890b2c07e0d68
Ma Yan <896098846@qq.com>
2014-07-07 18:19:47 +0900

    Project Created.

---
+++ My Space Shooting Game Prototype/My Space Shooting Game Prototype/BGMMyScene.h
@@ -0,0 +1,13 @@
+//
+//  BGMMyScene.h
+//  My Space Shooting Game Prototype
+//
+
+//  Copyright (c) 2014年 馬 岩. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface BGMMyScene : SKScene
+
+@end
---
+++ My Space Shooting Game Prototype/My Space Shooting Game PrototypeTests/My_Space_Shooting_Game_PrototypeTests.m
@@ -0,0 +1,34 @@
+//
+//  My_Space_Shooting_Game_PrototypeTests.m
+//  My Space Shooting Game PrototypeTests
+//
+//  Created by 馬 岩 on 14-7-7.
+//  Copyright (c) 2014年 馬 岩. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface My_Space_Shooting_Game_PrototypeTests : XCTestCase
+
+@end
+
+@implementation My_Space_Shooting_Game_PrototypeTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
